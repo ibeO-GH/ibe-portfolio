71cd65a2ea5024343c47d1cccd9f35c417f4ff8c
ibeO-GH <ibe.okorafor@gmail.com>
2026-04-06 14:02:51 +0100

    projects update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,32 +1,14 @@
 import ProjectCard from "../components/ProjectCard";
-import project1 from "../assets/financeDashb.png";
-import project2 from "../assets/veloraEcommerce.png";
-import project3 from "../assets/taskflow.png";
-import project4 from "../assets/devfind.png";
-import project5 from "../assets/healthguard.png";
-import project6 from "../assets/burgerD.png";
-import project7 from "../assets/weatherApp.png";
-import project8 from "../assets/neonlogist.png";
-import project9 from "../assets/NeonTaste.png";
-import project10 from "../assets/Tic_Tac_Toe.png";
-import project11 from "../assets/ToDoList.png";
-import project12 from "../assets/neonexcellence.png";
+import project1 from "../assets/veloraEcommerce.png";
+import project2 from "../assets/taskflow.png";
+import project3 from "../assets/devfind.png";
 
 const projects = [
-  {
-    title: "Finance Dashboard",
-    description:
-      "A clean and responsive finance dashboard for tracking income and expenses, designed with a focus on simplicity, usability, and real-time updates.",
-    image: project1,
-    demo: "https://finance-dashboard-six-woad.vercel.app/",
-    github: "https://github.com/ibeO-GH/Finance-Dashboard",
-    tech: ["React", "TypeScript", "Tailwind"],
-  },
   {
     title: "Velora – Modern E-commerce Frontend",
     description:
       "Modern e-commerce frontend with dynamic product detail pages with gallery, cart system with quantity control, persistent state (localStorage), and Responsive UI + animations.",
-    image: project2,
+    image: project1,
     demo: "https://velora-ecommerce-delta.vercel.app/",
     github: "https://github.com/ibeO-GH/Velora-ecommerce",
     tech: ["React", "TypeScript", "Tailwwind", "Context API"],
@@ -35,7 +17,7 @@ const projects = [
     title: "TaskFlow - Drag & Drop Task Manager",
     description:
       "A modern task management app featuring drag-and-drop functionality, task state transitions, and interactive UI for managing workflows efficiently.",
-    image: project3,
+    image: project2,
     demo: "https://taskflow-dnd.vercel.app/",
     github: "https://github.com/ibeO-GH/taskflow-dnd",
     tech: ["React", "TpeScript", "Tailwind", "dnd-kit"],
@@ -44,74 +26,10 @@ const projects = [
     title: "DevFinder - GitHub Explorer",
     description:
       "A modern GitHub user search application that fetches real-time data, displays developer profiles, repositories, and handles loading, error, and empty states with a polished UI.",
-    image: project4,
+    image: project3,
     demo: "https://dev-finder-app-taupe.vercel.app/",
     github: "https://github.com/ibeO-GH/DevFinder-App",
     tech: ["React", "TypeScript", "Tailwind", "GitHub API"],
-  },
-  {
-    title: "HealthCare Website",
-    description: "Responsive landing page for a healthcare service",
-    image: project5,
-    demo: "https://health-guard-rho.vercel.app",
-    github: "https://github.com/ibeO-GH/HealthGuard",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "Burger Builder",
-    description: "Interactive burger builder with drag-and-drop",
-    image: project6,
-    demo: "https://ibeo-gh.github.io/Burger-Delight/",
-    github: "https://github.com/ibeO-GH/Burger-Delight",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "Weather App",
-    description: "Real-time weather data using OpenWeatherMap API",
-    image: project7,
-    demo: "https://ibeo-gh.github.io/PRODIGY_WD_05/",
-    github: "https://github.com/ibeO-GH/PRODIGY_WD_05",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "NeonLogist",
-    description: "Logistics company website with modern design",
-    image: project8,
-    demo: "https://ibeo-gh.github.io/Neon-Logistics/",
-    github: "https://github.com/ibeO-GH/Neon-Logistics",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "NeonTaste",
-    description: "Food delivery service landing page",
-    image: project9,
-    demo: "https://ibeo-gh.github.io/PRODIGY_WD_01/",
-    github: "https://github.com/ibeO-GH/PRODIGY_WD_01",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "Tic Tac Toe",
-    description: "Classic tic tac toe game with two-player mode",
-    image: project10,
-    demo: "https://ibeo-gh.github.io/PRODIGY_WD_03/",
-    github: "https://github.com/ibeO-GH/PRODIGY_WD_03",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "ToDo App",
-    description: "Task management app with full CRUD functionality",
-    image: project11,
-    demo: "https://ibeo-gh.github.io/TODO-List-Project/",
-    github: "https://github.com/ibeO-GH/TODO-List-Project",
-    tech: ["HTML", "JavaScript", "CSS"],
-  },
-  {
-    title: "School Website",
-    description: "Multi-section school platform with modern UI",
-    image: project12,
-    demo: "https://ibeo-gh.github.io/Neon-Excellence/",
-    github: "https://github.com/ibeO-GH/Neon-Excellence",
-    tech: ["HTML", "JavaScript", "CSS"],
   },
 ];
 
